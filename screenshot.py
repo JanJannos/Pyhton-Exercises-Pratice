41681dce977da1355994cc59ce9b2ba572fc0077
JanJannosgit config --global user.email you@example.com <hanohj@gmail.com>
2020-12-03 13:20:43 +0200

    Added Buttons & UI

--- screenshot.py
+++ screenshot.py
@@ -8,7 +8,7 @@ def screenshot():
     name = int(round(time.time() * 1000))  # generate number
     name = 'screenshots/{}.png'.format(name)
     time.sleep(5)
-    # img = pyautogui.screenshot('test.png')
+    img = pyautogui.screenshot('test.png')
     img = pyautogui.screenshot(name)
     img.show()
 
@@ -18,3 +18,13 @@ def screenshot():
 root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
+
+# create 2 buttons
+
+button = tk.Button(frame, text="Take ScreenShot", command=screenshot)
+button.pack(side=tk.LEFT)
+close = tk.Button(frame, text="QUIT", command=quit)
+close.pack(side=tk.LEFT)
+
+# Run the UI
+root.mainloop()
